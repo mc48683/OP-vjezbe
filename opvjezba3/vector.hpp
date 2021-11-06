3ba4755839ad9f3a403839e831242380a08bafd3
mc48683 <92522084+mc48683@users.noreply.github.com>
2021-11-06 15:02:06 +0100

    Add files via upload

---
+++ opvjezba3/vector.hpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <vector>
+
+#include <algorithm>
+#include <string>
+
+using namespace std;
+
+void unos(vector<int>& v, int n);
+void unos1(vector<int>& v, int x, int y);
+void print(vector<int>& v, int n);
+void novi(vector<int>& v1, vector<int>& v2, vector<int>& v3);
+void sortiraj(vector<int>& v);
+void ukloni(vector<int>& v, int i);
+int podst(string s, string p);
+void rev_sort(vector <string>& niz);
